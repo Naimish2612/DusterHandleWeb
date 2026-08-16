321ba660e9413584460f1e3405bb0c6dbfaf495a
Naimish <mungara3524@live.com>
2026-08-17 00:41:23 +0530

    sccss budgets update, base url add.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 export const environment = {
   production: false,
   apiBaseUrls: {
-    common: 'http://localhost:7777/'
+    // common: 'http://localhost:7777/',
+    common: 'http://187.127.135.213:5090/',
   },
 };
